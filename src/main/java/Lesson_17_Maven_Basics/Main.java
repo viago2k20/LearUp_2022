86d482e0c0924433d88d8e7cc2f330563e045eb9
viago2k20 <denzh@mail.ru>
2022-01-31 16:54:36 +0300

    added homework description

--- src/main/java/Lesson_17_Maven_Basics/Main.java
+++ src/main/java/Lesson_17_Maven_Basics/Main.java
@@ -2,6 +2,8 @@ package Lesson_17_Maven_Basics;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("1. Создать Maven проект.");
+        System.out.println("2. Создать репозиторий на GitHub и загрузить в него Maven проект.");
+        System.out.println("3. Загрузить в репозиторий Maven проект.");
     }
 }
