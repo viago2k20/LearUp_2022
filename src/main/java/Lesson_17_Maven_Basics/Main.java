d349c195a2d6fce3aec45c5128a7e42956d74846
viago2k20 <denzh@mail.ru>
2022-01-31 17:04:48 +0300

    add discription about to move to the firstBranch

--- src/main/java/Lesson_17_Maven_Basics/Main.java
+++ src/main/java/Lesson_17_Maven_Basics/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("1. Создать Maven проект.");
         System.out.println("2. Создать репозиторий на GitHub и загрузить в него Maven проект.");
         System.out.println("3. Загрузить в репозиторий Maven проект.");
+        System.out.println("Переходим на новую ветку firstBranch!");
     }
 }
